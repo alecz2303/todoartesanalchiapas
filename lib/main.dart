1d1f2ea9c65e8d7be936e86f282a0811e0190b0f
Alejandro Rueda <alecz23@live.com>
2026-09-26 00:47:32 -0600

    chore: register bundled font licenses [TA-034]

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,21 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'screens/root_shell.dart';
 import 'theme/app_theme.dart';
 
 void main() {
+  LicenseRegistry.addLicense(() async* {
+    final fredokaLicense =
+        await rootBundle.loadString('assets/fonts/fredoka/OFL.txt');
+    yield LicenseEntryWithLineBreaks(['Fredoka'], fredokaLicense);
+
+    final nunitoLicense =
+        await rootBundle.loadString('assets/fonts/nunito/OFL.txt');
+    yield LicenseEntryWithLineBreaks(['Nunito'], nunitoLicense);
+  });
+
   runApp(const TodoArtesanalApp());
 }
 
